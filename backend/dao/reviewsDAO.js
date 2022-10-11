6a348121aaed202ab33b5d9ae6cb0e19fe119b37
rahulpatil8020 <rahuldpatil20@gmail.com>
2022-10-11 10:30:07 -0700

    Configuring backend

---
+++ backend/dao/reviewsDAO.js
@@ -0,0 +1,60 @@
+import mongodb from "mongodb";
+
+const ObjectId = mongodb.ObjectID;
+
+let reviews;
+
+export default class ReviewsDAO {
+  static async injectDB(conn) {
+    if (review) {
+      return;
+    }
+    try {
+      reviews = await conn.db(process.env.RESTREVIEWS_NS).collection("reviews");
+    } catch (e) {
+      console.error(`Unable to establish collection handles in userDAO: ${e}`);
+    }
+  }
+
+  static async addReview(restaurantId, user, review, data) {
+    try {
+      const reviewDoc = {
+        name: user.name,
+        user_id: user._id,
+        date: date,
+        text: review,
+        restaurant_id: ObjectId(restaurantId),
+      };
+      return await reviews.insertOne(reviewDoc);
+    } catch (e) {
+      console.error(`Unable to post review: ${e}`);
+      return { error: e };
+    }
+  }
+
+  static async updateReview(reviewId, userId, text, data) {
+    try {
+      const updateResponse = await review.updateOne(
+        { user_id: userId, _id: ObjectId(reviewId) },
+        { $set: { text: text, date: date } }
+      );
+      return updateResponse;
+    } catch (e) {
+      console.error(`Unable to update review: ${e}`);
+      return { error: e };
+    }
+  }
+
+  static async deleteReview(reviewId, userId) {
+    try {
+      const deleteResponse = await reviews.deleteOne({
+        _id: ObjectId(reviewId),
+        user_id: userId,
+      });
+      return deleteResponse;
+    } catch (e) {
+      console.error(`Unable to delete review: ${e}`);
+      return { error: e };
+    }
+  }
+}
