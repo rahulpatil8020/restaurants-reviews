1aeb519c73184759bd4d15671fb5789d97a3ab19
rahulpatil8020 <rahuldpatil20@gmail.com>
2022-10-29 11:17:54 -0700

    Some minor changes in backend

--- backend/api/reviews.controller.js
+++ backend/api/reviews.controller.js
@@ -3,14 +3,13 @@ import ReviewsDAO from "../dao/reviewsDAO.js";
 export default class ReviewsController {
   static async apiPostReview(req, res, next) {
     try {
-      const restaurantId = req.body.restaurant_id;
-      const review = req.body.text;
+      const restaurantId = req?.body?.restaurant_id;
+      const review = req?.body?.text;
       const userInfo = {
-        name: req.body.name,
-        _id: req.body.user_id,
+        name: req?.body?.name,
+        _id: req?.body?.user_id,
       };
       const date = new Date();
-
       const ReviewResponse = await ReviewsDAO.addReview(
         restaurantId,
         userInfo,
